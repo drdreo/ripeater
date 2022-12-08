f35ac3c9ddc43b181c29917a1454db439be4ed5c
DrDreo <code@anderas-hahn.at>
2022-12-08 22:26:21 +0100

    add broadcasting

--- go.mod
+++ go.mod
@@ -4,4 +4,13 @@ module fiber
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.33.0
+require (
+	github.com/gofiber/fiber/v2 v2.40.1
+	github.com/gofiber/websocket/v2 v2.1.2 // indirect
+	github.com/jaevor/go-nanoid v1.3.0 // indirect
+	github.com/klauspost/compress v1.15.12 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
+	github.com/valyala/fasthttp v1.43.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+
+	"github.com/jaevor/go-nanoid"
 )
+
+type Client struct {
+	Id         string
+	Connection *websocket.Conn
+}
+
+type ClientConnections map[string]Client
 
 func getPort() string {
 	port := os.Getenv("PORT")
@@ -17,14 +28,60 @@ func getPort() string {
 	return port
 }
 
+func connectHandler(connections *ClientConnections, connection *websocket.Conn) Client {
+	canonicID, _ := nanoid.Standard(21)
+	connId := canonicID()
+	log.Printf("New ws request %s", connId)
+	client := Client{Id: connId, Connection: connection}
+	(*connections)[connId] = client
+	return client
+}
+func disconnectHandler(connections *ClientConnections, client Client) {
+	log.Printf("disconnectHandler %s", client.Id)
+	delete((*connections), client.Id)
+}
+
 func main() {
+
+	clientConnections := make(ClientConnections)
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message": "Hello, Railway!",
+			"message": "Hello, from Ripeater!",
 		})
 	})
 
-	app.Listen(getPort())
+	// Access the websocket server: ws://localhost:3000/ws
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		client := connectHandler(&clientConnections, c)
+
+		c.SetCloseHandler(func(code int, text string) error {
+			disconnectHandler(&clientConnections, client)
+			return nil
+		})
+
+		for {
+			mtype, msg, err := c.ReadMessage()
+			if err != nil {
+				if websocket.IsCloseError(err) {
+					log.Println("Disonnected")
+				}
+				log.Println("Read:", err)
+				break
+			}
+
+			log.Printf("Incoming: %s", msg)
+
+			for _, cc := range clientConnections {
+				err = cc.Connection.WriteMessage(mtype, msg)
+				if err != nil {
+					log.Println("Write:", err)
+					break
+				}
+			}
+		}
+	}))
+
+	log.Fatal(app.Listen(getPort()))
 }
